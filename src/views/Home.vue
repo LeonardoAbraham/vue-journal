addd7c07f8ba52565c155d5bb3cf216c9df02c29
Leonardo <leon.alonzo.leo@gmail.com>
2022-02-21 15:47:59 -0600

    Estructura modular router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
 
-    <h1>Hola mundo</h1>
+        <h1>Hola mundo</h1>
 
-    <button class="btn btn-primary">primary</button>
-    <button class="btn btn-secondary">secondary</button>
-    <button class="btn btn-success">success</button>
-  </div>
+        <button @click="goToDaybook" class="btn btn-primary">primary</button>
+        <button @click="goToDaybook" class="btn btn-secondary">secondary</button>
+        <button @click="goToDaybook" class="btn btn-success">success</button>
+    </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        goToDaybook(){
+            this.$router.push({ name: 'daybook' })
+        }
+    },
+}
+</script>
 
