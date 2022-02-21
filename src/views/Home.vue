aa09ac488d1c67538b0ba8ad60098985a01a843e
Leonardo <leon.alonzo.leo@gmail.com>
2022-02-21 15:17:08 -0600

    instalación de bootstrap y sass

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    <h1>Hola mundo</h1>
+
+    <button class="btn btn-primary">primary</button>
+    <button class="btn btn-secondary">secondary</button>
+    <button class="btn btn-success">success</button>
   </div>
 </template>
 
