692399702a12421d1fa8f8b1fa0f5e2cb83206c1
Leonardo <sistemas05@interno.com.mx>
2022-05-05 13:22:00 -0500

    Preparar el camino para la inserción de registros

--- src/modules/daybook/views/NoEntrySelected.vue
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -2,14 +2,21 @@
     <div class="d-flex justify-content-center">
         <h1 class="align-self-center">No hay nada seleccionado</h1>
     </div>
-    <Fab />
+    <Fab 
+        @on:click="createNewEntry"
+    />
 </template>
 <script>
 import { defineAsyncComponent } from 'vue'
 export default {
     components:{
         Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
-    }
+    },
+    methods: {
+        createNewEntry(){
+            this.$router.push({ name:'entry', params: { id: 'new' } })
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
