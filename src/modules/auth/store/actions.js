58fd59eda8079a2439a7885d2dd197459e8e839c
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-04-04 15:57:11 -0600

    Actualizar el displayName del usuario

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -6,9 +6,9 @@ export const createUser = async ({ commit }, user) => {
     try {
         
         const { data } = await authApi.post(':signUp', { email, password, returnSecureToken: true })
-        console.log(data)
+        const { idToken, refreshToken } = data
 
-
+        await authApi.post(':update', { displayName: name, idToken })
         //TODO: Mutation: loginUser
 
         return { ok: true }
